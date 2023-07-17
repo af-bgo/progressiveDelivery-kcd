c3f171b2cdc20dc6ab328bc50199238e9e2a091b
Bhaskar Gogoi <bhaskar.gogoi@appsfactory.de>
2023-07-17 15:02:00 +0200

    Added new telemetry data

--- demoapp/app.js
+++ demoapp/app.js
@@ -29,3 +29,32 @@ app.get('/', async(req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+/**
+ * OpenTelemetry Code
+ */
+const { OpenTelemetryHook } = require('@openfeature/open-telemetry-hook');
+
+/*instrumentation.js*/
+const opentelemetry = require("@opentelemetry/sdk-node");
+const {getNodeAutoInstrumentations,} = require("@opentelemetry/auto-instrumentations-node");
+const {OTLPTraceExporter} = require("@opentelemetry/exporter-trace-otlp-grpc");
+const {OTLPMetricExporter} = require("@opentelemetry/exporter-metrics-otlp-grpc");
+const {PeriodicExportingMetricReader} = require('@opentelemetry/sdk-metrics');
+const {Resource} = require('@opentelemetry/resources');
+const {SemanticResourceAttributes} = require('@opentelemetry/semantic-conventions');
+
+const otelServiceName = process.env.OTEL_SERVICE_NAME || 'defaultService'
+
+const sdk = new opentelemetry.NodeSDK({
+  traceExporter: new OTLPTraceExporter({}),
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new OTLPMetricExporter({}),
+  }),
+  instrumentations: [getNodeAutoInstrumentations()],
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: otelServiceName
+  })
+});
+sdk.start();
