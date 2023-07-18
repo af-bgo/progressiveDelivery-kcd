9b8257577813e5e4b5a24114d99a962e10892403
Bhaskar Gogoi <bhaskar.gogoi@appsfactory.de>
2023-07-18 13:43:45 +0200

    Updated image and installed keptn to the new deployment

--- demoapp/buildImages.sh
+++ demoapp/buildImages.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker buildx create --use
-docker buildx build --platform linux/amd64,linux/arm64 --push --tag bgogoi/kcddemo:v1 .
+docker buildx build --platform linux/amd64,linux/arm64 --push --tag bgogoi24/kcddemo:v1 .
